b015a95cdd09416e715fee967acc802582e8b3fa
Matias Fanger <maattz@outlook.com>
2023-08-11 22:50:17 -0300

    add: extra columns

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,6 +6,7 @@ const db = useFirestore()
 
 const jobs = useCollection(collection(db, 'jobs'))
 const saas = useCollection(collection(db, 'saas'))
+const extra = useCollection(collection(db, 'extra'))
 
 const sortedJobs = computed(() => {
     return jobs.value.sort((a, b) => {
@@ -14,9 +15,8 @@ const sortedJobs = computed(() => {
 })
 
 useHead({
-    title: 'Matias Fanger - SSR Vue Developer',
-    // titleTemplate: (title) => `${title} | MySite`,
-    meta: [{ name: 'description', content: 'Matias Fanger - SSR Vue Developer Portfolio' }],
+    title: 'Matias Fanger - Software Engineer',
+    meta: [{ name: 'description', content: 'Matias Fanger - Software Engineer - Portfolio' }],
     link: [
         {
             rel: 'icon',
@@ -32,8 +32,7 @@ useHead({
         <SectionHero />
         <SectionSaas v-if="saas.length" :info="saas" />
         <SectionBlog v-if="sortedJobs.length" :info="sortedJobs" />
+        <SectionExtra v-if="extra.length" :info="extra" />
         <SectionFeatures />
     </div>
 </template>
-
-<style lang="postcss" scoped></style>
