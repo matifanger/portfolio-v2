7bc5fed50e8794c2dd2a9ef895574c48f399eac5
Matias Fanger <maattz@outlook.com>
2023-08-11 22:49:57 -0300

    fix: app.vue

--- app.vue
+++ app.vue
@@ -1,37 +1,9 @@
-<script lang="ts" setup>
-useHead({
-    title: 'Matias Fanger - SSR Vue Developer',
-    // titleTemplate: (title) => `${title} | MySite`,
-    meta: [{ name: 'description', content: 'Matias Fanger - SSR Vue Developer Portfolio' }],
-    // icon
-    link: [
-        {
-            rel: 'icon',
-            type: 'image/png',
-            href: '/logo.png',
-        },
-    ],
-})
-
-definePageMeta({
-    colorMode: 'dark',
-})
-</script>
-
 <template>
     <Html lang="en">
         <Body>
-            <SeoKit />
             <NuxtLayout>
-                <NuxtLoadingIndicator />
                 <NuxtPage />
             </NuxtLayout>
         </Body>
     </Html>
 </template>
-
-<style lang="postcss">
-body {
-    @apply bg-white text-gray-800 antialiased transition-colors duration-300;
-}
-</style>
