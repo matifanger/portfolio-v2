f5961e7165d7edfad6d0667c5ffe4835adf54731
Matias Fanger <maattz@outlook.com>
2024-11-06 19:52:36 -0300

    Update deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,70 +2,71 @@ import presetIcons from '@unocss/preset-icons'
 import { isProduction } from 'std-env'
 
 export default defineNuxtConfig({
-    srcDir: 'src',
-    ssr: true,
+  srcDir: 'src',
+  ssr: true,
+  modules: ['@vueuse/nuxt', '@nuxtjs/tailwindcss', '@unocss/nuxt', 'nuxt-vuefire'],
 
-    modules: ['@vueuse/nuxt', '@nuxtjs/tailwindcss', '@unocss/nuxt', 'nuxt-vuefire'],
+  postcss: {
+      plugins: {
+          'postcss-import': {},
+          'tailwindcss/nesting': {},
+          tailwindcss: {},
+          autoprefixer: {},
+      },
+  },
 
-    postcss: {
-        plugins: {
-            'postcss-import': {},
-            'tailwindcss/nesting': {},
-            tailwindcss: {},
-            autoprefixer: {},
-        },
-    },
+  vuefire: {
+      auth: false,
+      config: {
+          apiKey: process.env.FIREBASE_API_KEY,
+          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+          projectId: process.env.FIREBASE_PROJECTID,
+          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+          appId: process.env.FIREBASE_APP_ID,
+          measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+      },
+  },
 
-    vuefire: {
-        auth: false,
-        config: {
-            apiKey: process.env.FIREBASE_API_KEY,
-            authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-            projectId: process.env.FIREBASE_PROJECTID,
-            storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-            messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-            appId: process.env.FIREBASE_APP_ID,
-            measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-        },
-    },
+  unocss: {
+      uno: false,
+      preflight: false,
+      icons: true,
+      presets: [
+          presetIcons({
+              scale: 1.2,
+              extraProperties: {
+                  display: 'inline-block',
+              },
+          }),
+      ],
+  },
 
-    unocss: {
-        uno: false,
-        preflight: false,
-        icons: true,
-        presets: [
-            presetIcons({
-                scale: 1.2,
-                extraProperties: {
-                    display: 'inline-block',
-                },
-            }),
-        ],
-    },
+  runtimeConfig: {
+      public: {
+          google_analytics_id: process.env.FIREBASE_MEASUREMENT_ID,
+          production_mode: isProduction,
+      },
+  },
 
-    runtimeConfig: {
-        public: {
-            google_analytics_id: process.env.FIREBASE_MEASUREMENT_ID,
-            production_mode: isProduction,
-        },
-    },
+  css: ['@/assets/css/main.pcss', '@/assets/css/tailwind.css'],
 
-    css: ['@/assets/css/main.pcss', '@/assets/css/tailwind.css'],
+  vueuse: {
+      ssrHandlers: true,
+  },
 
-    vueuse: {
-        ssrHandlers: true,
-    },
+  sourcemap: {
+      server: true,
+      client: true,
+  },
 
-    sourcemap: {
-        server: true,
-        client: true,
-    },
+  vite: {
+      build: {
+          sourcemap: true,
+      },
+      clearScreen: true,
+      logLevel: 'info',
+  },
 
-    vite: {
-        build: {
-            sourcemap: true,
-        },
-        clearScreen: true,
-        logLevel: 'info',
-    },
+  compatibilityDate: '2024-11-06',
 })
